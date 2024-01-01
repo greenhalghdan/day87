df77d3e337ca52f65ff2fba11f247e6995c01956
greenhalghdan <49040634+greenhalghdan@users.noreply.github.com>
2024-01-01 19:17:35 +0000

    finished

--- ball.py
+++ ball.py
@@ -8,7 +8,7 @@ class Ball(Turtle):
         self.penup()
         self.x_move = 3
         self.y_move = 3
-        self.move_speed = 0.1
+        self.move_speed = 1
 
     def move_ball(self):
         new_x = self.xcor() + self.x_move
--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from turtle import Screen, Turtle
 from paddle import Paddle
 from block import Block
 from ball import Ball
-from time import sleep
+
+def user_alert(message):
+    ALIGNMENT = "center"
+    FONT = ("times new roman", 24, "normal")
+    alert = Turtle()
+    alert.hideturtle()
+    alert.color("white")
+    alert.write(message, align=ALIGNMENT, font=FONT)
 
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -15,28 +22,32 @@ screen.tracer(0)
 paddle = Paddle((0, -200))
 blocks = []
 block01 = Block((-200,200), "red")
-block04 = Block((-100,200),"red")
 block02 = Block((0,200), "red")
 block03 = Block((100,200),"red")
-block03 = Block((200,200),"red")
+block04 = Block((-100,200),"red")
+block05 = Block((200,200),"red")
+
+block11 = Block((-200,180), "orange")
+block12 = Block((0,180), "orange")
 block13 = Block((200,180),"orange")
 block14 = Block((100,180),"orange")
 block15 = Block((-100,180),"orange")
-block11 = Block((-200,180), "orange")
-block12 = Block((0,180), "orange")
+
+block21 = Block((-200,160), "yellow")
+block22 = Block((0,160), "yellow")
 block23 = Block((200,160),"yellow")
 block24 = Block((-100,160),"yellow")
 block25 = Block((100,160),"yellow")
-block21 = Block((-200,160), "yellow")
-block22 = Block((0,160), "yellow")
-block33 = Block((200,140),"green")
+
 block31 = Block((-200,140), "green")
+block32 = Block((0,140), "green")
+block33 = Block((200,140),"green")
 block34 = Block((-100,140), "green")
 block35 = Block((100,140), "green")
-block32 = Block((0,140), "green")
-block43 = Block((200,120),"blue")
+
 block41 = Block((-200,120), "blue")
 block42 = Block((0,120), "blue")
+block43 = Block((200,120),"blue")
 block44 = Block((100,120), "blue")
 block45 = Block((-100,120), "blue")
 
@@ -44,26 +55,31 @@ blocks.append(block01)
 blocks.append(block02)
 blocks.append(block03)
 blocks.append(block04)
+blocks.append(block05)
 
 blocks.append(block11)
 blocks.append(block12)
 blocks.append(block13)
 blocks.append(block14)
+blocks.append(block15)
 
 blocks.append(block21)
 blocks.append(block22)
 blocks.append(block23)
 blocks.append(block24)
+blocks.append(block25)
 
 blocks.append(block31)
 blocks.append(block32)
 blocks.append(block33)
 blocks.append(block34)
+blocks.append(block35)
 
 blocks.append(block41)
 blocks.append(block42)
-blocks.append(block45)
+blocks.append(block43)
 blocks.append(block44)
+blocks.append(block45)
 
 ball = Ball()
 screen.listen()
@@ -74,6 +90,7 @@ screen.onkey(paddle.move_right, "Right")
 playing = True
 
 while playing:
+    time.sleep(0.01)
     screen.update()
     ball.move_ball()
     if ball.ycor() > 250 or ball.ycor() < -250:
@@ -86,22 +103,19 @@ while playing:
         y = int(block.xcor())
         z = int(block.ycor())
         if w > z - 20 and w < z + 20 and x > y - 50 and x < y + 50:
-        #if ball.distance(block) < 50:
-            print(f"w: {w}")
-            print(f"x: {x}")
-            print(f"y: {y}")
-            print(f"z = {z}")
-            #time.sleep(10)
             ball.bounce_y()
-            #block.reset()
-            #block.color("white")
-            #time.sleep(2)
             block.goto(30000, 3000)
-            #time.sleep(2)
-    for block in blocks:
-        print(block.pos())
+            blocks.remove(block)
+            block._hidden_from_screen = True
     if ball.distance(paddle) < 50:
         ball.bounce_y()
+    if len(blocks) <= 0:
+        user_alert(message="You Win")
+        playing = False
+    if int(ball.ycor()) < -249:
+        user_alert(message="You Lose")
+        playing = False
+
 screen.update()
 screen.exitonclick()
 
